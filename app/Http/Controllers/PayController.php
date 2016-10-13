c5e21c81a981873843a8c8ebd50e058b2ccc00ef
MayZhang <1944786917@qq.com>
2016-10-13 14:19:35 +0800

    支付

--- app/Http/Controllers/PayController.php
+++ app/Http/Controllers/PayController.php
@@ -39,17 +39,24 @@ class PayController extends Controller
         $v['Amount'] = '';
         $v['Username'] = '';
         $v['AdditionalInfo'] = '';
-        $v['ReturnUrl'] = '/bfopay_repond';
+        $v['ReturnUrl'] = 'http://www3.goldmanyfx.com/bfopay_repond';
         $v['TerminalID'] = 19766;
         $v['TradeDate'] = date('YmdHis');
         $v['TransID'] = substr($v['TradeDate'],0,10).mt_rand(1000000000,9999999999);
         $Md5key="me6txh9l9ljec2mc";//md5密钥（KEY）
         $MARK = "|";
         $v['PageUrl']="https://pay.asiaswift.com/payindex";//借贷混合
+        $v['or'] = $v['MemberID'].$MARK.$v['PayID'].$MARK.$v['TradeDate'].$MARK.$v['TransID'].
+            $MARK.$v['OrderMoney'].$MARK.$v['PageUrl'].$MARK.$v['ReturnUrl'].$MARK.$v['NoticeType'].
+            $MARK.$Md5key;
         $v['Signature'] = md5($v['MemberID'].$MARK.$v['PayID'].$MARK.$v['TradeDate'].$MARK.$v['TransID'].
             $MARK.$v['OrderMoney'].$MARK.$v['PageUrl'].$MARK.$v['ReturnUrl'].$MARK.$v['NoticeType'].
             $MARK.$Md5key);
 
         return view('user.pay_recharge',compact('info','v'));
+    }
+
+    public function repond(){
+        
     }
 }
